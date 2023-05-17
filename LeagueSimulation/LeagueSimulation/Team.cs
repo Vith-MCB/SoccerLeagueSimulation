08fbfac6141f816cdd8453f972c22b87e70bedc8
Vitor Ribeiro <vitorriblacerda@gmail.com>
2023-05-17 16:04:06 -0300

    files prep and team class

---
+++ LeagueSimulation/LeagueSimulation/Team.cs
@@ -0,0 +1,29 @@
+﻿namespace LeagueSimulation;
+
+public class Team
+{
+    #region Team Info
+    public string Name { get; set; }
+    public double Strength { get; set; }
+    #endregion
+
+    #region Simulated data
+    public int points = 0, goalsMade = 0, goalsTaken = 0;
+    #endregion
+    
+    //Default constructor
+    public Team(){}
+    
+    //Team Constructor
+    public Team(string name, double strength)
+    {
+        this.Name = name;
+        this.Strength = strength;
+    }
+
+    #region Functions
+
+    public void CalculateNewStrength(double percentage){ Strength *= percentage; }
+
+    #endregion
+}
---
+++ LeagueSimulation/files/readme.txt.txt
@@ -0,0 +1,10 @@
+Here in this folder you can change data of the teams.
+
+If you want to write a new teams table make sure to follow this pattern:
+
+team_name,team_strength
+
+EX:
+
+Cruzeiro,72
+Corinthians,68
